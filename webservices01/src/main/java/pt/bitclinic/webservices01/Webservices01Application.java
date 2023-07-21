56c671ecba7f13301d15ab26417f66427a8f266f
amac81 <arnaldocanelas@gmail.com>
2023-07-21 12:27:55 +0100

    webservices01 1st commit

---
+++ webservices01/src/main/java/pt/bitclinic/webservices01/Webservices01Application.java
@@ -0,0 +1,13 @@
+package pt.bitclinic.webservices01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webservices01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webservices01Application.class, args);
+	}
+
+}
