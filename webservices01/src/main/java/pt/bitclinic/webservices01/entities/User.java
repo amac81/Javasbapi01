8db7a32c2b93cc2e773261bd697afb63d372a11e
amac81 <arnaldocanelas@gmail.com>
2023-07-21 18:44:26 +0100

    User change

--- webservices01/src/main/java/pt/bitclinic/webservices01/entities/User.java
+++ webservices01/src/main/java/pt/bitclinic/webservices01/entities/User.java
@@ -10,7 +10,7 @@ import jakarta.persistence.Id;
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	@Id
+	@Id 
 	private long id;
 	private String name;
 	private String email;
