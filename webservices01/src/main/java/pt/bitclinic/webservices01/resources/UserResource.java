4b5014ef568371a41d4351388228656a14425d06
amac81 <arnaldocanelas@gmail.com>
2023-07-25 10:15:55 +0100

    UserResource - insert - http code 201

--- webservices01/src/main/java/pt/bitclinic/webservices01/resources/UserResource.java
+++ webservices01/src/main/java/pt/bitclinic/webservices01/resources/UserResource.java
@@ -1,5 +1,6 @@
 package pt.bitclinic.webservices01.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import pt.bitclinic.webservices01.entities.User;
 import pt.bitclinic.webservices01.services.UserService;
@@ -23,23 +25,22 @@ public class UserResource {
 
 	@GetMapping
 	public ResponseEntity<List<User>> findAll() {
-		
 		List<User> userList = userService.findAll();
-
 		return ResponseEntity.ok().body(userList);
-
 	}
-	
+
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<User> findById(@PathVariable Long id) {
 		return ResponseEntity.ok().body(userService.findById(id));
 	}
 
-	
 	@PostMapping
-	public ResponseEntity<User> insert(@RequestBody	User user) {
+	public ResponseEntity<User> insert(@RequestBody User user) {
 		user = userService.insert(user);
-		return ResponseEntity.ok().body(user);
+		//to generate the correct HTTP response code 201 - Created
+		URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/{id}").buildAndExpand(user.getId())
+				.toUri();
+		return ResponseEntity.created(uri).body(user);
 	}
-	
+
 }
