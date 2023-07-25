270162d0247bb28c0be6dabb92486bc79fde7bdf
amac81 <arnaldocanelas@gmail.com>
2023-07-25 21:00:42 +0100

    Product: insert, delete, update

--- src/main/java/pt/bitclinic/webservices01/resources/ProductResource.java
+++ src/main/java/pt/bitclinic/webservices01/resources/ProductResource.java
@@ -1,13 +1,19 @@
 package pt.bitclinic.webservices01.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import pt.bitclinic.webservices01.entities.Product;
 import pt.bitclinic.webservices01.services.ProductService;
@@ -29,7 +35,28 @@ public class ProductResource {
 	public ResponseEntity<Product> findById(@PathVariable Long id) {
 		return ResponseEntity.ok().body(productService.findById(id));
 	}
-	
-	//TODO ProductResource: @PostMapping insert, @DeleteMapping delete, @PutMapping update
+		
+	@PostMapping
+	public ResponseEntity<Product> insert(@RequestBody Product obj) {
+		obj = productService.insert(obj);
+		// to generate the correct HTTP response code 201 - Created
+		URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/products/{id}").buildAndExpand(obj.getId())
+				.toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		productService.delete(id);
+		// generate HTTP response code 204 - No Content
+		return ResponseEntity.noContent().build();
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Product> update(@PathVariable Long id, @RequestBody Product obj) {
+		obj = productService.update(id, obj);
+		return ResponseEntity.ok().body(obj);
+	}
+
 
 }
--- src/main/java/pt/bitclinic/webservices01/services/ProductService.java
+++ src/main/java/pt/bitclinic/webservices01/services/ProductService.java
@@ -4,11 +4,15 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import jakarta.persistence.EntityNotFoundException;
 import pt.bitclinic.webservices01.entities.Product;
 import pt.bitclinic.webservices01.repositories.ProductRepository;
+import pt.bitclinic.webservices01.services.exceptions.DatabaseException;
 import pt.bitclinic.webservices01.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -34,7 +38,41 @@ public class ProductService {
 			throw new ResourceNotFoundException("Product with Id [" + id + "] not found.");
 		}
 	}
+		
+	public Product insert(Product obj) {
+		return productRepository.save(obj);
+	}
 	
-	//TODO ProductService: insert, delete and update methods
+	public void delete(Long id) {
+		try {
+			productRepository.deleteById(id);
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		catch(DataIntegrityViolationException e1) {
+			throw new DatabaseException(e1.getMessage());		
+		}
+	}
+
+	public Product update(Long id, Product obj) {
+		try {
+			//getReferenceById more efficient than findById
+			//getReferenceById only "prepares" the monitored object 
+			Product entity = productRepository.getReferenceById(id);
+			updateData(entity, obj);
+			return productRepository.save(entity);
+			
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}	
+	}
+	
+	private void updateData(Product entity, Product obj) {		
+		entity.setName(obj.getName());
+		entity.setDescription(obj.getDescription());
+		entity.setPrice(obj.getPrice());
+		entity.setImgUrl(obj.getImgUrl());
+	}
+	
 
 }
